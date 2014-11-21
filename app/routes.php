02de4291a4fd96f74db3d9dac5f92a0f47b5fba3
Jeremy L. Grajales <jeremygrajales@gmail.com>
2014-11-21 11:18:37 -0600

    Updated routes.

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,28 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+/** ------------------------------------------
+ *  Route model binding
+ *  ------------------------------------------
+ */
+Route::model('appointment', 'Appointment');
+
+/** ------------------------------------------
+ *  Route constraint patterns
+ *  ------------------------------------------
+ */
+Route::pattern('appointment', '[0-9]+');
+
+
+Route::get('appointment/create', array('as' => 'appointment.create', 'uses' => 'AppointmentsController@create'));
+Route::post('appointment/create', array('as' => 'appointment.create', 'uses' => 'AppointmentsController@create'));
+
+Route::get('appointment/{appointment}/show', array('as' => 'appointment.show', 'uses' => 'AppointmentsController@show'));
+
+Route::get('appointment/show-all', array('as' => 'appointment.show.all', 'uses' => 'AppointmentsController@showAll'));
+
+Route::get('appointment/{appointment}/edit', array('as' => 'appointment.edit', 'uses' => 'AppointmentsController@edit'));
+Route::post('appointment/{appointment}/edit', array('as' => 'appointment.edit', 'uses' => 'AppointmentsController@edit'));
+
+Route::get('appointment/{appointment}/status/edit', array('as' => 'appointment.status.edit', 'uses' => 'AppointmentsController@statusEdit'));
+Route::post('appointment/{appointment}/status/edit', array('as' => 'appointment.status.edit', 'uses' => 'AppointmentsController@statusEdit'));
